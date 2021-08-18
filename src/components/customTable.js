2828ca148aac9fe61551f24d2a1abfc84d7c6781
Swastik Sharma <swastik@bidec.xyz>
2021-08-18 20:32:41 +0530

    Minor table fetch fix

--- src/components/customTable.js
+++ src/components/customTable.js
@@ -9,57 +9,54 @@ const CustomTable = () =>{
 
     useEffect( ()=>{
         getTableData();
-    },[getTableData])
+    },[])
 
     if(isLoading){
         return (
             <Table className={`${classes.customTable} text-center`}>
                 <thead>
-                    <tr>
-                        <th>...</th>
+                    <tr key="thead1">
+                        <th key="loaderHead">...</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <td></td>
+                    <tr key="thead2">
+                        <td key="loaderD"></td>
                     </tr>
                 </tbody>
             </Table>
         )
     }
-    if(!isLoading){
-        return (
-            <Table className={classes.customTable}>
-                <thead>
-                    <tr>
-                        {value['headers'].map(( listValue, index ) => {
-                            return (
-                                <th>{listValue}</th>
-                            );
-                        })}
-                    </tr>
-                </thead>
-                <tbody>
-                    {value['data'].map(( listValue, index ) => {
+    
+    return (
+        <Table className={classes.customTable}>
+            <thead>
+                <tr key="thead">
+                    {value['headers'].map(( listValue, index ) => {
                         return (
-                        <tr key={index}>
-                            <td>{listValue[0]}</td>        
-                            <td>{listValue[1]}</td>        
-                            <td>{listValue[2]}</td>        
-                            <td>{listValue[3]}</td>        
-                            <td>{listValue[4]}</td>        
-                            <td>{listValue[5]}</td>        
-                            <td>{listValue[6]}</td>        
-                            <td>{listValue[7]}</td>  
-                        </tr>
-                    );
+                            <th key={`Head${index}`}>{listValue}</th>
+                        );
                     })}
-                </tbody>
-            </Table>
-        )
-    }
-    
-    
+                </tr>
+            </thead>
+            <tbody>
+                {value['data'].map(( listValue, index ) => {
+                    return (
+                    <tr key={index}>
+                        <td key={`0d`}>{listValue[0]}</td>        
+                        <td key={`1d`}>{listValue[1]}</td>        
+                        <td key={`2d`}>{listValue[2]}</td>        
+                        <td key={`3d`}>{listValue[3]}</td>        
+                        <td key={`4d`}>{listValue[4]}</td>        
+                        <td key={`5d`}>{listValue[5]}</td>        
+                        <td key={`6d`}>{listValue[6]}</td>        
+                        <td key={`7d`}>{listValue[7]}</td>  
+                    </tr>
+                );
+                })}
+            </tbody>
+        </Table>
+    ) 
 }
 
 export default CustomTable;
